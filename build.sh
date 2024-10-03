5284815f33081de936a68db1a6690e8f4c7a9c49
lucas kluber <lucas.kluber@gmail.com>
2024-10-03 14:58:32 -0300

    - Adicionar docker

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install docker
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
@@ -21,3 +21,15 @@ rpm-ostree install screen
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable docker.socket
+
+## Instal cockpit
+rpm-ostree install cockpit-system cockpit-ostree \
+                    cockpit-podman cockpit-networkmanager \
+                    cockpit-files cockpit-cloudflared
+systemctl enable cockpit.service 
+
+# echo 'PasswordAuthentication yes' | sudo tee /etc/ssh/sshd_config.d/02-enable-passwords.conf
+# sudo systemctl try-restart sshd
+# podman container runlabel --name cockpit-ws RUN quay.io/cockpit/ws
+# podman container runlabel INSTALL quay.io/cockpit/ws
